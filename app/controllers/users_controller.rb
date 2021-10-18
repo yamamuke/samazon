3bfb6503dde4cfe1a7b1cce8b91f5cf307e22327
yamamuke <yamamukeisuke@gmail.com>
2021-10-18 15:04:08 +0000

    16 edit_password function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user
+  before_action :authenticate_user!
   
   def edit
   end
@@ -14,7 +15,21 @@ class UsersController < ApplicationController
   
   def edit_address
   end
-
+  
+  def update_password
+    if password_set?
+      @user.update_password(user_params) 
+      flash[:notice] = "パスワードは正しく更新されました。"
+      redirect_to root_url
+    else
+      @user.errors.add(:password, "パスワードに不備があります。")
+      render "edit_password"
+    end
+    
+    def edit_password
+    end
+  end
+  
   private
     def set_user
       @user = current_user
@@ -22,5 +37,10 @@ class UsersController < ApplicationController
     
     def user_params
       params.permit(:name, :email, :address, :phone, :password, :password_confirmation)
+    end
+    
+    def password_set?
+      user_params[:password].present? && user_params[:password_confirmation].present? ?
+      true : false
     end
 end
